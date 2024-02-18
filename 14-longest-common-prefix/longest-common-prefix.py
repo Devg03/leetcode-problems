99225a8e4e1e4ad115959571c62199f8c4d0751b
Dev Gandhi <gandhiDX02@gmail.com>
2024-02-17 20:23:03 -0500

    Time: 35 ms (79.12%) | Memory: 16.6 MB (70.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,12 +3,11 @@ class Solution:
         smallest = min(strs)
         result = ""
 
-        for i in range(0, 1):
-            for j in range(0, len(smallest)):
-                if smallest[j] == max(strs)[j]:
-                    result += smallest[j]
-                else:
-                    break
+        for j in range(0, len(smallest)):
+            if smallest[j] == max(strs)[j]:
+                result += smallest[j]
+            else:
+                break
 
         return result
         
