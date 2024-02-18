cd7a327e34eeb4e117632c958313ef0ce259dd92
Dev Gandhi <gandhiDX02@gmail.com>
2024-02-17 20:24:55 -0500

    Time: 42 ms (36.23%) | Memory: 16.6 MB (70.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,10 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        smallest = min(strs)
+        largest = max(strs)
         result = ""
 
-        for j in range(0, len(min(strs))):
-            if min(strs)[j] == max(strs)[j]:
-                result += min(strs)[j]
+        for j in range(0, len(smallest)):
+            if smallest[j] == largest[j]:
+                result += smallest[j]
             else:
                 break
 
