e84f498e166d7a8588875aa3cf2c43e96a54f9af
Dev Gandhi <gandhiDX02@gmail.com>
2024-03-17 16:37:28 -0400

    Time: 34 ms (67.58%) | Memory: 16.5 MB (94.10%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,7 +1,6 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         result = ""
-
         minSize = min(len(word1), len(word2))
 
         counter = 0
@@ -9,9 +8,11 @@ class Solution:
             result += word1[counter]
             result += word2[counter]
             counter += 1
+
         if (len(word1) > len(word2)):
             for i in range(minSize, len(word1)):
                 result += word1[i]
+        
         if (len(word2) > len(word1)):
             for i in range(minSize, len(word2)):
                 result += word2[i]
