66688e5cab43c5b996e0b6b845739b7d95129365
Dev Gandhi <gandhiDX02@gmail.com>
2024-03-17 16:17:37 -0400

    Time: 36 ms (95.62%) | Memory: 16.6 MB (49.19%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        return reduce(max, [s.count(" ") + 1 for s in sentences])
+        return max([s.count(" ") for s in sentences]) + 1
