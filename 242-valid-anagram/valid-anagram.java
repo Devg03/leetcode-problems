bd3577a7a0217a19b706fca2584858774fb479be
Dev Gandhi <gandhiDX02@gmail.com>
2026-02-21 00:59:16 -0500

    Time: 5 ms (58.99%) | Memory: 44.2 MB (90.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,13 +5,10 @@ class Solution {
         int[] freqTable = new int[26];
         for (int i = 0; i < s.length(); i++) {
             freqTable[s.charAt(i) - 'a']++;
+            freqTable[t.charAt(i) - 'a']--;
         }
-
-        for (int j = 0; j < t.length(); j++) {
-            freqTable[t.charAt(j) - 'a']--;
-            if (freqTable[t.charAt(j) - 'a'] < 0) {
-                return false;
-            }
+        for (int count = 0; count < 25; count++) {
+            if (freqTable[count] != 0) return false;
         }
 
         return true;
